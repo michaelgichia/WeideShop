d1e9994d0257376c3d32ea47b441aaee47c47a3e
Michael Gichia <mqyynm@gmail.com>
2016-11-02 12:19:13 +0300

    Fixed catalogue url.

--- weideshop/products/urls.py
+++ weideshop/products/urls.py
@@ -4,8 +4,7 @@ from .views import CatalogueListView, ProductDetailView, SubcategoryListView, Ca
 
 urlpatterns = [
 	url(r'^$', CategoryListView.as_view(), name='category'),
-    url(r'^(?P<slug>)/$', SubcategoryListView.as_view(), name='sub-category'),   
     url(r'^catalog/$', CatalogueListView.as_view(), name='category_catalog'),
-    url(r'^(?P<slug>[-\w]+)/$', ProductDetailView.as_view(), name='detail'),
-
+    url(r'^(?P<slug>[-\w]+)/$', SubcategoryListView.as_view(), name='sub-category'),   
+    url(r'^catalog/(?P<slug>[-\w]+)/$', ProductDetailView.as_view(), name='detail'),
 ] 
