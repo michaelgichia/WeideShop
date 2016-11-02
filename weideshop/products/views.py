1055b2d95ef649b180f449bcda706774b0ed9ee7
Michael Gichia <mqyynm@gmail.com>
2016-11-02 22:36:03 +0300

    Created a subcategory view, e.g Vehicle > cars.

--- weideshop/products/views.py
+++ weideshop/products/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
 from django.utils import timezone
 
 from .models import Product, Subcategory, Category 
 
-# Create your views here.
+
 class CategoryListView(ListView):
 	"""
 	Browse all products in the categories.
@@ -14,28 +14,45 @@ class CategoryListView(ListView):
 	template_name = 'products/category_list.html'
 
 	def get_queryset(self):
+		"""
+		Returns all categories.
+		"""
 		return Category.objects.get_queryset().all()
+
 
 class SubcategoryListView(ListView):
 	"""
 	Browse all products in the catalogue.
 	"""
 	model = Subcategory
+	template_name = 'products/subcategory_list.html'
 
-	def get_queryset(self, **kwargs):
-		return Subcategory.objects.get_queryset().all()
+	def get_queryset(self):
+		"""
+		Returns all sub-categories.
+		"""
+		category = get_object_or_404(Category, slug = self.kwargs.get('slug'))
+		return Subcategory.objects.filter(category = category)
 
 
 class CatalogueListView(ListView):
-	'''List all products'''
+	"""
+	Display all products in the db.
+	"""
 
 	model = Product
 
-	def get_context_data(self, **kwargs):
-		context = super(CatalogueListView, self).get_context_data(**kwargs)
-		return context
+	def get_queryset(self):
+		"""
+		Returns all categories.
+		"""
+		return Product.objects.get_queryset().all()
+		
 
 class ProductDetailView(DetailView):
+	"""
+	Display individual products details
+	"""
 
 	model = Product
 
