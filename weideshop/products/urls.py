256aea26c756bc2dda6f4ae88802c22170ca5d65
Michael Gichia <mqyynm@gmail.com>
2016-11-06 23:32:13 +0300

    Fixed slug error for product item.

--- weideshop/products/urls.py
+++ weideshop/products/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	url(r'^$', CategoryListView.as_view(), name='categories'),
     url(r'^(?P<category_slug>[-\w]+)/$', SubcategoryListView.as_view(), name='sub-category'),
 	url(r'^(?P<category_slug>[-\w]+)/(?P<subcategory_slug>[-\w]+)/$', ProductListView.as_view(), name='product-list'),
-	url(r'^(?P<category_slug>[-\w]+)/(?P<subcategory_slug>[-\w]+)/(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product-detail'),
+	url(r'^(?P<category_slug>[-\w]+)/(?P<subcategory_slug>[-\w]+)/(?P<product_slug>[-\w]+)/$', ProductDetailView.as_view(), name='product-detail'),
 
 ] 
