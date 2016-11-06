83c2d34eac6cd19c0b6eeb761c490f43112e108e
Michael Gichia <mqyynm@gmail.com>
2016-11-06 23:31:14 +0300

    Linkend to product_list template.

--- weideshop/products/views.py
+++ weideshop/products/views.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# Third party stuff
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import TemplateView
-from django.views.generic.list import MultipleObjectMixin
 
+# Our stuff
 from .models import Product, Subcategory, Category 
 
 
@@ -57,6 +60,21 @@ class ProductListView(ListView):
 	template_name = 'products/product_list.html'
 	context_object_name = "Product list"
 
+	def get_context_data(self, **kwargs):
+		"""
+		Returns self.category_slug and self.subcategory)slug needed 
+		on the product_list.html as a
+		one of the {% url %} slug params.
+		"""
+		context = super(ProductListView, self).get_context_data(**kwargs)
+		# Get category_slug
+		context['categories'] = Category.objects.all()
+		context['category_slug'] = self.kwargs.get('category_slug')
+		# Get subcategory_slug
+		context['subcategories'] = Subcategory.objects.all()
+		context['subcategory_slug'] = self.kwargs.get('subcategory_slug')
+		return context
+
 	def get_queryset(self):
 		"""
 		Browse all products under selected subcategory.
@@ -64,6 +82,17 @@ class ProductListView(ListView):
 		self.sub_category = get_object_or_404(Subcategory, subcategory_slug = self.kwargs.get('subcategory_slug'))
 		return Product.objects.filter(sub_category = self.sub_category)
 
+
+class ProductDetailView(DetailView):
+	"""
+	Display individual products details
+	"""
+	model = Product
+
+	def get_object(self):
+		object = get_object_or_404(Product, product_slug=self.kwargs['product_slug'])
+		return object 
+	
 
 class CatalogueListView(ListView):
 	"""
@@ -77,12 +106,6 @@ class CatalogueListView(ListView):
 		"""
 		return Product.objects.get_queryset().all()
 
-class ProductDetailView(DetailView):
-	"""
-	Display individual products details
-	"""
-	model = Product
- 
 	
 class CatalogueDetailView(DetailView):
 	"""
@@ -93,14 +116,12 @@ class CatalogueDetailView(DetailView):
 	slug_field = 'product_slug'
 
 	def get_object(self):
-		# Call the superclass
+		"""
+		Call the superclass
+		"""
 		object = super(CatalogueDetailView, self).get_object()
 		return object
 
-
-	# model = Product
-	# template_name = 'products/product_detail.html'
-	# slug_field = 'product_slug'
 	
 
 
